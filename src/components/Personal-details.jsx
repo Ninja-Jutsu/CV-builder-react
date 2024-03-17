4a3abe7767cc746c1e89be16cb7eb63721e5cf26
Ninja-Jutsu <ninja.jutsu.way@gmail.com>
2024-03-18 04:41:07 +0800

    Learn more React

--- src/components/Personal-details.jsx
+++ src/components/Personal-details.jsx
@@ -1,63 +1,109 @@
 import React, { useState } from "react";
 
-function MakeForm({ value, onChange, type }) {
+function MakeForm({ field, value, onChange, type }) {
     const [isValidated, setValidate] = useState(true)
     return (
-        <form action="#" className="emailForm">
-            {isValidated ? (
-                <>
-                    <div id={{ value } + "Div"}>
-                        <input
-                            id={value}
-                            type={type}
-                            value={value}
-                            onChange={onChange}
-                        />
-                        <label htmlFor={value}>{type}</label>
+        <header>
+            <form action="#" className="emailForm">
+                {isValidated ? (
+                    <>  <div className="inputSection">
+                        <div id={type + "Div"}>
+                            <input
+                                id={value}
+                                type={type}
+                                value={value}
+                                onChange={onChange}
+                                placeholder={field}
+                            />
+                        </div>
+                        <button
+                            onClick={() => setValidate(false)}>
+                            Validate
+                        </button>
                     </div>
-                    <button
-                        onClick={() => setValidate(false)}>
-                        Validate
-                    </button>
-                </>
-            ) : (
-                <div>
-                    <h2>{value}</h2>
-                    <button
-                        onClick={() => setValidate(true)}>
-                        Edit
-                    </button>
-                </div>
+                    </>
+                ) : (
+                    <div id="validated">
 
-            )}
-        </form>
+                        <button
+                            onClick={() => setValidate(true)}>
+                            Edit
+                        </button>
+                        <p>{field}: {value}</p>
+                    </div>
+                )}
+            </form>
+        </header>
     )
 }
 
 
 export default function DisplayForm() {
-    const [email, setEmail] = useState('')
-    const [name, setName] = useState('')
+    const [email, setEmail] = useState("")
+    const [name, setName] = useState("")
+    const [address, setAddress] = useState("")
+    const [mobile, setMobile] = useState("")
+    const [dateOfBirth, setBirth] = useState("")
+    const [availability, setAvailability] = useState("")
 
 
-    function handleEmailChange(e) {
-        setEmail(e.target.value)
-    }
 
     function handleNameChange(e) {
         setName(e.target.value)
     }
+    function handleEmailChange(e) {
+        setEmail(e.target.value)
+    }
+    function handleAddressChange(e) {
+        setAddress(e.target.value)
+    }
+    function handleMobileChange(e) {
+        setMobile(e.target.value)
+    }
+    function handleBirthChange(e) {
+        setBirth(e.target.value)
+    }
+    function handleAvailabilityChange(e) {
+        setAvailability(e.target.value)
+    }
+
     return (
         <>
             <MakeForm
+                field={'Email Address'}
                 value={email}
                 type={'email'}
                 onChange={handleEmailChange}
             />
             <MakeForm
+                field={'Name'}
                 value={name}
                 type={'text'}
                 onChange={handleNameChange}
+            />
+            <MakeForm
+                field={'Phone Number'}
+                value={mobile}
+                type={'text'}
+                onChange={handleMobileChange}
+            />
+            <MakeForm
+                field={'Home Address'}
+                value={address}
+                type={'text'}
+                onChange={handleAddressChange}
+            />
+            <MakeForm
+                field={'Date of Birth'}
+                value={dateOfBirth}
+                type={'text'}
+                onChange={handleBirthChange}
+            />
+            <MakeForm
+                field={'Availability'}
+                value={availability}
+                type={'text'}
+                onChange={handleAvailabilityChange}
             />
         </>
     )
